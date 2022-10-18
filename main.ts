b1ec7721527157b78489342d0628d6c09f29a42b
Henk Timmermans <scanhesi@gmail.com>
2022-10-18 17:08:10 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -7,19 +7,3 @@ basic.forever(function () {
         basic.showString("safe~")
     }
 })
-basic.forever(function () {
-    FireV = pins.analogReadPin(AnalogPin.P0)
-    if (FireV < 100) {
-        basic.showString("Fire!")
-    } else {
-        basic.showString("safe~")
-    }
-})
-basic.forever(function () {
-    FireV = pins.analogReadPin(AnalogPin.P0)
-    if (FireV < 100) {
-        basic.showString("Fire!")
-    } else {
-        basic.showString("safe~")
-    }
-})
