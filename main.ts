a3823d871b34d4f3aaebaaf605518ca3b099474b
Henk Timmermans <scanhesi@gmail.com>
2022-10-18 17:06:27 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,25 @@
+let FireV = 0
+basic.forever(function () {
+    FireV = pins.analogReadPin(AnalogPin.P0)
+    if (FireV < 100) {
+        basic.showString("Fire!")
+    } else {
+        basic.showString("safe~")
+    }
+})
+basic.forever(function () {
+    FireV = pins.analogReadPin(AnalogPin.P0)
+    if (FireV < 100) {
+        basic.showString("Fire!")
+    } else {
+        basic.showString("safe~")
+    }
+})
+basic.forever(function () {
+    FireV = pins.analogReadPin(AnalogPin.P0)
+    if (FireV < 100) {
+        basic.showString("Fire!")
+    } else {
+        basic.showString("safe~")
+    }
+})
